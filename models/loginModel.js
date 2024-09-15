ff9bc21d1c03c865bea7569c9a763a1c29855eea
disururathnayake <disuru.office@gmail.com>
2024-09-15 13:39:59 +1000

    project setup

--- models/loginModel.js
+++ models/loginModel.js
@@ -1,8 +1,10 @@
+const { v4: uuidv4 } = require('uuid');
 const client = require('../dbConnection'); 
 
 const collection = client.db().collection('users'); 
 
 function createUser(user, callback) {
+    user.userId = uuidv4();
     collection.insertOne(user, callback); // Function to insert a new user
 }
 
